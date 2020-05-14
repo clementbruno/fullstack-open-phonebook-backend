68e937b3d5e13a1a006784d9f76f5aeebfce0e3d
clementbruno <clmnt.bruno@gmail.com>
2020-05-14 13:47:38 +0200

    uibuild

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fullstack Open 2020
+
+The code is deployed on heroku and is accessible at the following address: "https://intense-taiga-62830.herokuapp.com/"
+
+The code in this repository includes the backend express server and the front-end build. (The code of the front-end repository is located in the main repository https://github.com/clementbruno/fullstack-open-2020/part3/phonebook-front-end-part3)
---
+++ build/precache-manifest.0b729f1c6cd65de5b1bede8064a8585d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb76d422bdd528bbc526caaa380edd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82fbc3e208167d89aff2",
+    "url": "/static/css/main.7c8ffaed.chunk.css"
+  },
+  {
+    "revision": "e50feacd6b0434bcdba7",
+    "url": "/static/js/2.1eaabf53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1eaabf53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82fbc3e208167d89aff2",
+    "url": "/static/js/main.0b849937.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
